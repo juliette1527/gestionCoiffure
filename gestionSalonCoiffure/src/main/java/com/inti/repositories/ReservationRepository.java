0089e3447ed81a856afd080e0b2b6aa7d0911e2d
Malorf <cedric.rahbari@gmail.com>
2023-05-25 17:06:28 +0200

    Salon_Reservation_Prestation

--- gestionSalonCoiffure/src/main/java/com/inti/repositories/ReservationRepository.java
+++ gestionSalonCoiffure/src/main/java/com/inti/repositories/ReservationRepository.java
@@ -3,9 +3,9 @@ package com.inti.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.inti.entities.Role;
+import com.inti.entities.Reservation;
 
 @Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
 }
